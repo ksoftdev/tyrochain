0ffda5d0322e93d70e373ff65b2e530368efea6f
ksoftdev <kuntursoftdev@gmail.com>
2021-04-25 11:17:14 -0500

    Minning reward and transactions

--- chain.js
+++ chain.js
@@ -1,9 +1,14 @@
 const { Block } = require('./block');
+const { Transaction } = require('./transaction');
 
 class Chain{
+
     constructor(){
         this.chain = [this.createGenesisBlock()];
         this.difficulty = 4;
+        this.height = 0;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
 
     createGenesisBlock(){
@@ -14,10 +19,62 @@ class Chain{
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock){
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+    minePendingTransactions(miningRewardAddress){
+        this.height++;
+
+        let block = new Block(this.height, Date.now(), this.pendingTransactions);
+        block.mineBlock(this.difficulty);
+
+        this.chain.push(block);
+        console.log('Block successfully mined!!');
+
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAddress, this.miningReward)
+        ];
+
+        console.log('New miner transfer to: ', miningRewardAddress, ' of value: ', this.miningReward);
+    }
+
+    createTransaction(tx){
+        this.pendingTransactions.push(tx);
+    }
+
+    getBalanceOfAddress(address){
+        let balance = 0;
+
+        for(const block of this.chain){
+            for(const trans of block.transactions){
+                if(trans.fromAddress === address ){
+                    balance -= trans.amount;
+                }
+
+                if(trans.toAddress === address){
+                    balance += trans.amount;
+                }
+            }
+        }
+
+        return balance;
+    }
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if(!currentBlock.hasValidTransactions()){
+                return false;
+            }
+
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+        }
+        return true;
     }
 }
 
--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const { Block } = require('./block');
 const { Chain } = require('./chain');
 
+const { Transaction } = require('./transaction');
+
 tyrocoin = new Chain();
 
-console.log('Minning block 1...');
-tyrocoin.addBlock(new Block(1, Date.now(), {from: 'Bob', to: 'Alice', amount: 50}));
+tyrocoin.createTransaction(new Transaction('Bob', 'Alice', 100));
+tyrocoin.createTransaction(new Transaction('Alice', 'Bob', 10));
 
-console.log('Minning block 2...');
-tyrocoin.addBlock(new Block(2, Date.now(), {from: 'Alice', to: 'Grace', amount: 15}));
+console.log('\n Starting the miner...');
+tyrocoin.minePendingTransactions('Cesar');
+
+console.log('\nBalance of Cesar is', tyrocoin.getBalanceOfAddress('Cesar'));
+
+console.log('\n Starting the miner again...');
+tyrocoin.minePendingTransactions('Cesar');
+
+console.log('\nBalance of Cesar is', tyrocoin.getBalanceOfAddress('Cesar'));
 
 console.log(JSON.stringify(tyrocoin, null, 4));
